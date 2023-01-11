ea1ecc14d75587c38aa305b2384edd3e2125b8a4
windymelt <windymelt@3qe.us>
2023-01-12 03:23:12 +0900

    can synt

--- src/main/scala/com/github/windymelt/voicevoxcore4s/Core.scala
+++ src/main/scala/com/github/windymelt/voicevoxcore4s/Core.scala
@@ -1,11 +1,29 @@
 package com.github.windymelt.voicevoxcore4s
 
-import com.sun.jna.Library;
-import com.sun.jna.Native;
+import com.sun.jna.Library
+import com.sun.jna.Native
+import com.sun.jna.ptr.IntByReference
+import com.sun.jna.Pointer
 
+// cf. https://github.com/java-native-access/jna/blob/master/www/Mappings.md
 // cf. https://github.com/VOICEVOX/voicevox_core/blob/0.13.0/core/src/core.h
 trait Core extends Library {
   def initialize(use_gpu : Boolean, cpu_num_threads: Int = 0, load_all_models: Boolean = true): Boolean
+  // int64_t -> Long
+  def load_model(speaker_id: Long): Boolean
+  def is_model_loaded(speaker_id: Long): Boolean
+  // def finalize(): Unit // TODO: solve name conflict
+  def metas(): String // JSON
+  def supported_devices(): String // JSON
+  def yukarin_s_forward(length: Long, phoneme_list: Array[Long], speaker_id: Array[Long], output: Array[Float]): Boolean
+  // TODO: yukarin_sa_forward
+  // TODO: decode_forward
+  def last_error_message(): String
+  def voicevox_load_openjtalk_dict(dict_path: String): Int // => VoicevoxResultCode
+  def voicevox_tts(text: String, speaker_id: Long, output_binary_size: Array[Int]/* == IntByReference */ /* int* */, output_wav: Pointer)
+  // TODO: voicevox_tts_from_kana
+  def voicevox_wav_free(wav: Array[Byte]): Unit // should wrap this array
+  def voicevox_error_result_to_message(result_code: Int): String
 }
 
 object Core {
--- src/main/scala/com/github/windymelt/voicevoxcore4s/Main.scala
+++ src/main/scala/com/github/windymelt/voicevoxcore4s/Main.scala
@@ -1,12 +1,30 @@
 package com.github.windymelt.voicevoxcore4s
 
+import com.sun.jna.Pointer
+import com.sun.jna.Memory
+import com.sun.jna.Native
+
 object Hello extends App {
   println("initializing")
  // System.load("/usr/local/lib/libonnxruntime.so")
   //System.load("/usr/local/lib/libcore.so")
   println(System.getProperty("java.library.path"))
   val core = Core()
-  val initialized = core.initialize(true)
-  println(s"Hello, voicevoxcore4s! ($initialized)")
+  val initialized = core.initialize(use_gpu = false)
+  println(s"Hello, voicevoxcore4s! initialized? -> ($initialized)")
+  if (initialized) {
+    println("reading meta info...")
+    println(core.metas())
+    // めたんは2
+    val length = Array[Int](42)
+    val resultPtr = new Memory(Native.POINTER_SIZE).getPointer(0)
+    val loadDictResult = core.voicevox_load_openjtalk_dict("open_jtalk_dic_utf_8-1.11")
+    println(s"loadResult: ${loadDictResult}")
+    core.voicevox_tts("こんにちは、世界", 2L, length, resultPtr)
+    println(s"length: ${length.head}")
+    println(s"err: ${core.last_error_message()}")
+    val resultArray = resultPtr.getByteArray(0, length.head)
+    core.voicevox_wav_free(resultArray) // FIXME
+  }
 }
 
