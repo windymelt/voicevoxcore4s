a73a6b68771cf64911088312e2dd0d830d1383b8
Windymelt <windymelt@3qe.us>
2023-01-11 22:15:15 +0900

    working

--- binding/src/main/scala/com/github/windymelt/voicevoxcore4s/Main.scala
+++ binding/src/main/scala/com/github/windymelt/voicevoxcore4s/Main.scala
@@ -2,6 +2,8 @@ package com.github.windymelt.voicevoxcore4s
 
 object Hello extends App {
   println("initializing")
+  System.load("/usr/local/lib/libonnxruntime.so")
+  System.load("/usr/local/lib/libcore.so")
   println(System.getProperty("java.library.path"))
   val core = new Core()
   val initialized = core.initialize(true)
